cddf53575f0518c4c40235e143044c8f25b5f2ab
Alies Lapatsin <web.lapateen@gmail.com>
2024-03-12 01:02:06 +0100

    Support Laravel 11

--- tests/HtmlCodeTest.php
+++ tests/HtmlCodeTest.php
@@ -3,10 +3,11 @@
 namespace InteractionDesignFoundation\NovaHtmlCodeField\Tests;
 
 use InteractionDesignFoundation\NovaHtmlCodeField\HtmlCode;
+use PHPUnit\Framework\Attributes\Test;
 
 final class HtmlCodeTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_throws_an_exception_if_invalid_template_used(): void
     {
         $this->expectException(\InvalidArgumentException::class);
